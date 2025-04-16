028124297b294b9923b41f34f2bce53dcddf73af
Darshan Dhameliya <darshandhameliya18@gmail.com>
2025-04-16 19:31:06 +0530

    Write script for get linkdin data using Langchain

--- GenAI/Prompt Engineering/LangChain/output_parser.py
+++ GenAI/Prompt Engineering/LangChain/output_parser.py
@@ -11,3 +11,17 @@ class SummaryModelLinkdin(BaseModel):
         return {"summary": self.summary, "facts": self.facts, "key_skills": self.key_skills}
 
 summary_parser_linkedin = PydanticOutputParser(pydantic_object=SummaryModelLinkdin)
+
+
+class MyInfo(BaseModel):
+    technologies:List = Field(description="Technologies which i know", title="technology")
+
+    def to_dict(self)->Dict[str, Any]:
+        return {"technology": self.technology}
+    
+    @classmethod
+    def model_json_schema(cls) -> Dict[str, Any]:
+        return cls.schema()
+    
+
+summary_parser_my_info = PydanticOutputParser(pydantic_object=MyInfo)
